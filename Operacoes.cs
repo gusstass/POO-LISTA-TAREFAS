1327438ba90f4541b80f390b7cd9e9a5fd26cb1c
gusstass <gustavosasi@uni9.edu.br>
2025-10-07 00:12:52 +0000

    feat: subindo arquivos

---
+++ Operacoes.cs
@@ -0,0 +1,27 @@
+public class Operacoes
+{
+    public int Criar(Tarefa tarefa)
+    {
+        return 0;
+    }
+
+    public Tarefa Buscar(int id)
+    {
+        return null;
+    }
+
+    public list<Tarefa> Listar()
+    {
+        return Array.Empty<Tarefa>();
+    }
+
+    public void Alterar(Tarefa tarefa)
+    {
+
+    }
+
+    public void Excluir(int id)
+    {
+        
+    }
+}
