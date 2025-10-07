777cfe735dc1f7a0b40d2477218065c1e2b221ac
gusstass <gustavosasi@uni9.edu.br>
2025-10-07 00:51:03 +0000

    feat: subindo arquivos

--- Operacoes.cs
+++ Operacoes.cs
@@ -1,8 +1,27 @@
 public class Operacoes
 {
+    private string connectionstring = "";
+
     public int Criar(Tarefa tarefa)
     {
-        return 0;
+        using (var conexao = new MySqlConnection(connectionstring))
+        {
+            conexao.Open();
+            string sql = @"insert into tarefa(nome, descricao, dataCriacao, status, dataExecucao)
+                        values (@nome, @descricao, @dataCriacao, @status, @dataExecucao);
+                        select LAST_INSERT_ID();";
+            using (var cmd = new MySqlCommand(sql, conexao))
+            {
+                cmd.Parameters.AddWithValue("@nome", tarefa.Nome);
+                cmd.Parameters.AddWithValue("@descricao", tarefa.Descricao);
+                cmd.Parameters.AddWithValue("@dataCricao", tarefa.DataCriacao);
+                cmd.Parameters.AddWithValue("@status", tarefa.Status);
+                cmd.Parameters.AddWithValue("@dataExecucao", tarefa.DataExecucao);
+
+                return Convert.Toint32(cmd.ExecuteScalar());
+            }
+        }
+        //return 0;
     }
 
     public Tarefa Buscar(int id)
@@ -22,6 +41,6 @@ public class Operacoes
 
     public void Excluir(int id)
     {
-        
+
     }
 }
